ccc952c0fd28b7c0262935adaee9c6cbd3b6c116
Molunerfinn <marksz@teamsz.xyz>
2018-05-11 18:07:42 +0800

    Fixed: test

--- test/observer-pattern.spec.ts
+++ test/observer-pattern.spec.ts
@@ -19,6 +19,17 @@ test('Should fire observers as expected', () => {
   expect(observer2.subject).toBe('Done')
 })
 
+test('Should fire different observers as expected', () => {
+  let observer1 = new op.Observer('click')
+  let observer2 = new op.Observer('hover')
+  let subjects = new op.Subject()
+  subjects.add('click', observer1)
+  subjects.add('hover', observer2)
+  subjects.fire('click')
+  expect(observer1.subject).toBe('Done')
+  expect(observer2.subject).toBe('hover')
+})
+
 test('Should remove observer as expected', () => {
   let observer = new op.Observer('click')
   let subjects = new op.Subject()
@@ -27,25 +38,3 @@ test('Should remove observer as expected', () => {
   subjects.fire('click')
   expect(observer.subject).toBe('click')
 })
-
-// let a: any = {}
-
-// if (!a['click']) {
-//   a['click'] = 1
-//   console.log(a)
-// }
-
-// test('Should return Sunday when inputing 20171028', () => {
-//   let result = dayOfWeek('20171028')
-//   expect(result).toBe('Saturday')
-// })
-
-// test('Should return Tuesday when inputing 20170103', () => {
-//   let result = dayOfWeek('20170103')
-//   expect(result).toBe('Tuesday')
-// })
-
-// test('Should return Wednesday when inputing 20301106', () => {
-//   let result = dayOfWeek('20301106')
-//   expect(result).toBe('Wednesday')
-// })
